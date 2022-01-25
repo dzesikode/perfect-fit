f998b730a1ddd1c3a2bb82707e0934db665ef1d5
dzes483 <jban@sdf.org>
2022-01-25 20:41:56 +0200

    Create account models & managers

--- back-end/accounts/models.py
+++ back-end/accounts/models.py
@@ -1,3 +1,52 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+from .managers import CustomUserManager
+
+
+class CustomUser(AbstractUser):
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, primary_key=True)
+    mailing_list = models.BooleanField()
+
+
+@receiver(post_save, sender=CustomUser)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created and not instance.is_staff:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def save_user_profile(sender, instance, **kwargs):
+    if not instance.is_staff:
+        instance.profile.save()
+
+
+class Address(models.Model):
+    ADDRESS_TYPE_CHOICES = [
+        (1, 'Shipping'),
+        (2, 'Billing')
+    ]
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    primary = models.BooleanField()
+    type = models.IntegerField(choices=ADDRESS_TYPE_CHOICES)
+    line_1 = models.CharField(max_length=120)
+    line_2 = models.CharField(max_length=60)
+    city = models.CharField(max_length=60)
+    state = models.CharField(max_length=3)
+    zip = models.CharField(max_length=15)
