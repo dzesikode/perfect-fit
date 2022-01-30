99321a15af4d1c828de7689a984501469be95104
dzes483 <jban@sdf.org>
2022-01-30 13:18:50 +0200

    Create serializers for account models

---
+++ back-end/accounts/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import CustomUser, Profile, Address
+
+
+class AddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Address
+        fields = ['id', 'profile', 'primary', 'type', 'line_1', 'line_2', 'city', 'state', 'zip']
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'password']
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    addresses = AddressSerializer(many=True, required=False)
+
+    class Meta:
+        model = Profile
+        fields = ['user', 'mailing_list', 'addresses']
