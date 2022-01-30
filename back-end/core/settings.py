b9c8c864eddb39b6de09ef878264bbc6e1395f4d
dzes483 <jban@sdf.org>
2022-01-30 13:20:18 +0200

    Add Django rest framework

--- back-end/core/settings.py
+++ back-end/core/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Application definition
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'accounts',
     'store'
 ]
