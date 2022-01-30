92c460dae1ae7c7efd0f5f2e04835bf511ad54fe
dzes483 <jban@sdf.org>
2022-01-30 13:19:46 +0200

    Create viewsets for account models

--- back-end/accounts/views.py
+++ back-end/accounts/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from .models import CustomUser, Address, Profile
+from rest_framework import viewsets
+from .serializers import UserSerializer, AddressSerializer, ProfileSerializer
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows profiles to be viewed or edited.
+    """
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+
+class AddressViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows addresses to be viewed or edited.
+    """
+    queryset = Address.objects.all()
+    serializer_class = AddressSerializer
