329a18c542842b18a13e2df684491c33c26a0486
dzes483 <jban@sdf.org>
2022-02-02 22:01:08 +0200

    Initial implementation of store views

--- back-end/store/views.py
+++ back-end/store/views.py
@@ -1,3 +1,48 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import *
+from .models import Brand, Color, Category, SubCategory, Product, Variant, PromoCode, Order, OrderItem
 
-# Create your views here.
+
+class BrandViewSet(viewsets.ModelViewSet):
+    queryset = Brand.objects.all()
+    serializer_class = BrandSerializer
+
+
+class ColorViewSet(viewsets.ModelViewSet):
+    queryset = Color.objects.all()
+    serializer_class = ColorSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SubCategoryViewSet(viewsets.ModelViewSet):
+    queryset = SubCategory.objects.all()
+    serializer_class = SubCategorySerializer
+
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class VariantViewSet(viewsets.ModelViewSet):
+    queryset = Variant.objects.all()
+    serializer_class = VariantSerializer
+
+
+class PromoCodeViewSet(viewsets.ModelViewSet):
+    queryset = PromoCode.objects.all()
+    serializer_class = PromoCodeSerializer
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+
+
+class OrderItemViewSet(viewsets.ModelViewSet):
+    queryset = OrderItem.objects.all()
+    serializer_class = OrderItemSerializer
