ad95f39dbdb2733496a300b52cf19718561bd42e
dzes483 <jban@sdf.org>
2022-02-11 22:10:05 +0200

    Add trailing slashes

--- back-end/core/urls.py
+++ back-end/core/urls.py
@@ -17,26 +17,26 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     # Public
-    path('api/brands', BrandListView.as_view()),
-    path('api/categories', CategoryListView.as_view()),
-    path('api/products', ProductListView.as_view()),
-    path('api/orders', OrderListCreateView.as_view()),  # api/orders/{user}
-    path('api/orders/<pk>', OrderEditView.as_view()),  # api/orders/{user}/{pk}
+    path('api/brands/', BrandListView.as_view()),
+    path('api/categories/', CategoryListView.as_view()),
+    path('api/products/', ProductListView.as_view()),
+    path('api/orders/', OrderListCreateView.as_view()),  # api/orders/{user}
+    path('api/orders/<pk>/', OrderEditView.as_view()),  # api/orders/{user}/{pk}
     # Brands
-    path('api/admin/brands', BrandCreateView.as_view()),
-    path('api/admin/brands/<pk>', BrandEditDeleteView.as_view()),
+    path('api/admin/brands/', BrandCreateView.as_view()),
+    path('api/admin/brands/<pk>/', BrandEditDeleteView.as_view()),
     # Categories
-    path('api/admin/categories', CategoryCreateView.as_view()),
-    path('api/admin/categories/<pk>', CategoryEditDeleteView.as_view()),
+    path('api/admin/categories/', CategoryCreateView.as_view()),
+    path('api/admin/categories/<pk>/', CategoryEditDeleteView.as_view()),
     # Orders
-    path('api/admin/orders/<pk>', OrderRetrieveEditDeleteView.as_view()),
-    path('api/admin/orders', OrderListCreateView.as_view()),
+    path('api/admin/orders/<pk>/', OrderRetrieveEditDeleteView.as_view()),
+    path('api/admin/orders/', OrderListCreateView.as_view()),
     # Products
-    path('api/admin/products', ProductCreateView.as_view()),
-    path('api/admin/products/<pk>', ProductEditDeleteView.as_view()),
+    path('api/admin/products/', ProductCreateView.as_view()),
+    path('api/admin/products/<pk>/', ProductEditDeleteView.as_view()),
     # Promo Codes
-    path('api/admin/promo_codes', PromoCodeListCreateView.as_view()),
-    path('api/admin/promo_codes/<pk>', PromoCodeEditDeleteView.as_view()),
+    path('api/admin/promo_codes/', PromoCodeListCreateView.as_view()),
+    path('api/admin/promo_codes/<pk>/', PromoCodeEditDeleteView.as_view()),
     # Other
     path('api-auth/', include('rest_framework.urls')),
 ]
