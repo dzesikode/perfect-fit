c86f20437a82b68334ac12521f99e82986de0fb7
dzes483 <jban@sdf.org>
2022-03-24 18:29:30 +0200

    Tidy logic in tasks

--- back-end/store/tasks.py
+++ back-end/store/tasks.py
@@ -5,12 +5,12 @@ from django.utils import timezone
 
 logger = get_task_logger(__name__)
 
+
 @shared_task
 def check_for_expired_promo_codes():
     now = timezone.now()
     active_promo_codes = PromoCode.objects.filter(active=True)
     for promo_code in active_promo_codes:
-        if promo_code.expiration_date:
-            if promo_code.expiration_date < now:
-                promo_code.active = False
-                promo_code.save()
+        if promo_code.expiration_date and promo_code.expiration_date < now:
+            promo_code.active = False
+            promo_code.save()
