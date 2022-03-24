ddbb3177df48933690691cd34f5beb2b294431bf
dzes483 <jban@sdf.org>
2022-03-24 21:02:31 +0200

    Handle password serialization

--- back-end/accounts/serializers.py
+++ back-end/accounts/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from .models import Address, User
+from django.contrib.auth.hashers import make_password
 
 
 class AddressSerializer(serializers.ModelSerializer):
@@ -11,4 +12,15 @@ class AddressSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'password', 'addresses', 'is_on_mailing_list']
+        fields = ['id', 'username', 'email', 'password', 'addresses', 'is_on_mailing_list', 'given_name', 'family_name']
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data.get('password'))
+        return super(UserSerializer, self).create(validated_data)
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        if 'password' in validated_data:
+            user.set_password(validated_data['password'])
+            user.save()
+        return user
